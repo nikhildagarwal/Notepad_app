b91871cea4f4f5f11becd28316f8383d4b823b8f
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-10-02 00:05:29 -0400

    db changes

--- test.py
+++ test.py
@@ -2,7 +2,8 @@ import sqlite3
 import scripts
 from scripts import create_note, add_user, update_note,fetch_notes, fetch_user
 
-users = ['email.nikhil.agarwal@gmail.com','hopefunfun@gmail.com','nda49@scarletmail.rutgers.edu']
+users = ['email.nikhil.agarwal@gmail.com','hopefunfun@gmail.com','nda49@scarletmail.rutgers.edu',
+         'as2689@scarletmail.rutgers.edu','ak7.shrivastava@gmail.com']
 
 for user in users:
     fu = fetch_user.FetchUser(user,"")
